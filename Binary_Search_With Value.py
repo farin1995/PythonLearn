2ca80aca4472b5a78f1abb8840b376929f864a42
farin1995 <farin.sab3@gmail.com>
2025-07-09 00:29:16 +0530

    Signed-off-by: farin1995 <farin.sab3@gmail.com>

--- Binary_Search_With Value.py
+++ Binary_Search_With Value.py
@@ -1,23 +1,36 @@
-def binary_search(arr, target):
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    
 
-    arr.vals = []
-    arr.index = 0
+    while low <= high:
 
-    if arr[0] == target:
-        return 0
-    elif arr[0] < target:
-        return binary_search(arr, target)
+        mid = (high + low) // 2
 
-    else:
-        return binary_search_recursive(arr, target, left, mid - 1)
+        # If x is greater, ignore left half
+        if arr[mid] < x:
+            low = mid + 1
 
-arr = [1, 3, 5, 7, 9, 11]
-target = 7
+        # If x is smaller, ignore right half
+        elif arr[mid] > x:
+            high = mid - 1
 
-# Iterative
-result = binary_search(arr, target)
-print("Iterative: Found at index", result)
+        # means x is present at mid
+        else:
+            return mid
 
-# Recursive
-result = binary_search_recursive(arr, target, 0, len(arr) - 1)
-print("Recursive: Found at index", result)
+    # If we reach here, then the element was not present
+    return -1
+
+
+# Test array
+arr = [ 2, 3, 4, 10, 40 ]
+x = 10
+
+# Function call
+result = binary_search(arr, x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")

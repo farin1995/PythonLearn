25074e032955a2a3337063589f62a41557d0283c
farin1995 <farin.sab3@gmail.com>
2025-07-09 00:39:51 +0530

    Fib series

---
+++ Fibonacci_Series.py
@@ -0,0 +1,9 @@
+def fibonacci_loop(n):
+    fib_series = [0, 1]
+    for i in range(2, n):
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series[:n]
+
+# Example usage
+n = 10
+print(fibonacci_loop(n))
